af69fcff894f3591fac8a0e21e5d459505ba812d
akira-cn <19511344@qq.com>
2022-07-20 19:39:58 +0800

    update

--- index.js
+++ index.js
@@ -10,8 +10,7 @@ const koaBody = require('koa-body');
 
 // TODO kv 服务说明
 router.get('/', async (ctx, next) => {
-  const res = await db.list();
-  ctx.body = res;
+  ctx.throw(403, JSON.stringify({reason: 'forbidden'}));
 });
 
 // 鉴权 + 获得 projectID
@@ -38,10 +37,7 @@ router.put('/doc/:id', auth, async (ctx, next) => {
   } catch(ex) {
     ctx.throw(403, JSON.stringify({reason: ex.message}));
   }
-});
-
-// post
-router.post('/doc/:id', auth, async (ctx, next) => {
+}).post('/doc/:id', auth, async (ctx, next) => {
   const id = `${ctx.params.id}.${ctx._projectId}`;
   const data = ctx.request.body;
   try {
@@ -51,10 +47,7 @@ router.post('/doc/:id', auth, async (ctx, next) => {
   } catch(ex) {
     ctx.throw(403, JSON.stringify({reason: ex.message}));
   }
-});
-
-// delete
-router.del('/doc/:id', auth, async (ctx, next) => {
+}).del('/doc/:id', auth, async (ctx, next) => {
   const id = `${ctx.params.id}.${ctx._projectId}`;
   try {
     const {_rev} = await db.get(id);
@@ -63,10 +56,7 @@ router.del('/doc/:id', auth, async (ctx, next) => {
   } catch(ex) {
     ctx.throw(404, JSON.stringify({reason: ex.message}));
   }
-});
-
-// get
-router.get('/doc/:id', auth, async (ctx, next) => {
+}).get('/doc/:id', auth, async (ctx, next) => {
   try {
     const id = `${ctx.params.id}.${ctx._projectId}`;
     const info = await db.get(id);
